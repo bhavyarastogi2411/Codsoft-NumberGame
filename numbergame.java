470892d4ab294308b50596d53e3adcb8ad618fcb
Bhavya Rastogi <bhavsrasto2408@gmail.com>
2023-08-04 20:11:18 +0530

    numberjava

--- numbergame.java
+++ numbergame.java
@@ -1,49 +1,54 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Random;
 
-public class WordCounter {
+public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int lowerLimit = 1;
+        int upperLimit = 100;
+        int maxAttempts = 10;
+        boolean playAgain = true;
+        int roundsPlayed = 0;
+        int totalAttempts = 0;
 
-        System.out.println("Welcome to Word Counter!");
-        System.out.print("Enter 'text' to input text manually or 'file' to provide a file: ");
-        String choice = scanner.nextLine().trim();
+        System.out.println("Welcome to the Guess the Number game!");
 
-        if (choice.equalsIgnoreCase("text")) {
-            System.out.print("Enter the text: ");
-            String inputText = scanner.nextLine();
-            int wordCount = countWords(inputText);
-            System.out.println("Total words: " + wordCount);
-        } else if (choice.equalsIgnoreCase("file")) {
-            System.out.print("Enter the file path: ");
-            String filePath = scanner.nextLine();
-            try {
-                String fileContent = readFile(filePath);
-                int wordCount = countWords(fileContent);
-                System.out.println("Total words: " + wordCount);
-            } catch (FileNotFoundException e) {
-                System.out.println("File not found.");
+        while (playAgain) {
+            roundsPlayed++;
+            int targetNumber = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            int attempts = 0;
+
+            System.out.println("\nRound " + roundsPlayed + ":");
+            System.out.println("Guess a number between " + lowerLimit + " and " + upperLimit + ".");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else if (userGuess > targetNumber) {
+                    System.out.println("Too high! Try again.");
+                } else {
+                    System.out.println("Congratulations! You guessed the number " + targetNumber + " in " + attempts + " attempts.");
+                    totalAttempts += attempts;
+                    break;
+                }
             }
-        } else {
-            System.out.println("Invalid choice.");
+
+            if (attempts == maxAttempts) {
+                System.out.println("Oops! You've reached the maximum number of attempts. The correct number was " + targetNumber + ".");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next();
+            playAgain = playAgainInput.equalsIgnoreCase("yes");
         }
 
+        System.out.println("\nGame over! You played " + roundsPlayed + " rounds and your average number of attempts per round was " + (double) totalAttempts / roundsPlayed);
         scanner.close();
-    }
-
-    public static int countWords(String text) {
-        String[] words = text.split("\\s+");
-        return words.length;
-    }
-
-    public static String readFile(String filePath) throws FileNotFoundException {
-        StringBuilder content = new StringBuilder();
-        try (Scanner fileScanner = new Scanner(new File(filePath))) {
-            while (fileScanner.hasNextLine()) {
-                content.append(fileScanner.nextLine()).append("\n");
-            }
-        }
-        return content.toString();
     }
 }
